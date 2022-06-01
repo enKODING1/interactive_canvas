ec44031f2ee2b396eb2d07fd3dfcba5b96ae4949
Guest <guest-kjys3f@h11156.sgb.skir.kr>
2022-06-01 23:18:41 +0900

    fix:bar crash

--- crash/Shape.js
+++ crash/Shape.js
@@ -1,74 +1,125 @@
-export class Shape{
-    constructor(x,y,radius,speed,color){
+export class Shape {
+    constructor(x, y, radius, speed, color) {
         this.x = x;
         this.y = y;
         this.sx = speed;
         this.sy = speed;
-        this.radius =  radius;
+        this.radius = radius;
         this.speed = speed;
         this.color = color;
-        this.width = Math.floor((Math.random()*100 ) + 20);
-       this.height = Math.floor((Math.random()*100 ) + 20);
+        this.width = Math.floor((Math.random() * 100) + 20);
+        this.height = Math.floor((Math.random() * 100) + 20);
     }
     //원으로 그리는 옵션
-    draw(ctx){
+    draw(ctx) {
         this.x += this.sx;
         this.y += this.sy;
 
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         this.crash();
-   }
-   //사각형으로 그리는 옵션
-   drawRect(ctx){
-       this.x += this.sx;
-       this.y += this.sy; 
-       
-
-       ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.rect(this.x,this.y,this.width,this.height);
-        ctx.fill();
-        this.crash();
-
-   }
-
-   crash(){
-       let minX = this.radius;
-       let minY = this.radius;
-
-       let maxX = window.innerWidth - this.radius;
-       let maxY = window.innerHeight - this.radius;
-
-       if(this.x > maxX || this.x < minX){
-           this.sx = -this.sx;
-       }else if(this.y > maxY || this.y < minY){
-           this.sy =  -this.sy;
-       }
-
-
-       
-   }
-
-
-    crashBar(barX,barY,barWidth,barHeight){
-    let top = window.innerHeight  ;
-    let bottom = window.innerHeight;
-
-    let start_left = barX;
-    let bottom_left = barX + barHeight;
-
-    let start_right = barX + barWidth;
-    let bottom_right = start_right + barHeight;
-
-    if(this.x > top){
-        this.sx = -this.sx;
+        this.crashBar();
     }
-   
-    
+    //사각형으로 그리는 옵션
+    drawRect(ctx) {
+        //    this.x += this.sx;
+        //    this.y += this.sy; 
 
-}
-  
+
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+        this.crash();
+        this.crashBar();
+
+    }
+
+    crash() {
+        let minX = this.radius;
+        let minY = this.radius;
+
+        let maxX = window.innerWidth - this.radius;
+        let maxY = window.innerHeight - this.radius;
+
+        if (this.x > maxX || this.x < minX) {
+            this.sx *= -1;
+            this.x += this.sx;
+        } else if (this.y > maxY || this.y < minY) {
+            this.sy *= -1;
+            this.y += this.sy;
+        }
+
+
+    }
+
+
+    crashBar(x, y, width, height) {
+
+
+
+
+        let minX = x - this.radius;
+        let maxX = x + width + this.radius;
+
+        let minY = y - this.radius;
+        let maxY = y + height + this.radius;
+
+        if (this.x > minX && this.x < maxX && this.y > minY && this.y < maxY) {
+            const x1 = Math.abs(minX - this.x);
+            const x2 = Math.abs(this.x - maxX);
+            const y1 = Math.abs(minY - this.y);
+            const y2 = Math.abs(this.y - maxY);
+
+            const min1 = Math.min(x1, x2);
+            const min2 = Math.min(y1, y2);
+            const min = Math.min(min1, min2);
+
+            if (min == min1) {
+                this.sx *= -1;
+                this.x += this.sx;
+            } else if (min == min2) {
+                this.sy *= -1;
+                this.y += this.sy;
+            }
+        }
+
+
+
+
+
+
+
+
+        // let minX = x - this.radius;
+        // let maxX = x + width + this.radius;
+
+        // let minY = y - this.radius;
+        // let maxY = y+height+ this.radius;
+
+        // if(this.x > minX && this.x <maxX && this.y > minY && this.y < maxY){
+        //     const x1 = Math.abs(minX - this.x);
+        //     const x2 = Math.abs(this.x - maxX);
+        //     const y1 = Math.abs(minY - this.y);
+        //     const y2 = Math.abs(this.y-maxY);
+
+        //     const min1 = Math.min(x1,x2);
+        //     const min2 = Math.min(y1,y2);
+        //     const min = Math.min(min1,min2);
+
+        //     if(min == min1){
+        //         this.sx *= -1;
+        //         this.x += this.sx;
+        //     }else if(min == min2){
+        //         this.sy *= -1;
+        //         this.y += this.sy;
+        //     }
+        // }
+
+
+
+
+    }
 }
--- crash/app.js
+++ crash/app.js
@@ -1,83 +1,95 @@
-import {Shape} from './Shape.js';
-import {Bar} from './Bar.js';
+import { Shape } from './Shape.js';
+import { Bar } from './Bar.js';
 
-class App{
-    constructor(){
+class App {
+    constructor() {
         this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
 
-        window.addEventListener('resize',this.resize.bind(this));
+        window.addEventListener('resize', this.resize.bind(this));
         this.resize();
 
         window.requestAnimationFrame(this.animate.bind(this));
 
-      //bar
+        //bar
         this.bar_width = 150;
         this.bar_height = 80;
-        this.bar_x = window.innerWidth/2 - this.bar_width/2;
-        this.bar_y = window.innerHeight - this.bar_height;
+        this.bar_x = window.innerWidth / 2 - this.bar_width / 2;
+        this.bar_y = (window.innerHeight - this.bar_height) - 100;
         this.bar_range = this.bar_x;
-        
-        this.bar = new Bar(this.bar_x,this.bar_y,this.bar_width,this.bar_height,this.bar_range);
-        window.addEventListener('keydown',(e)=>{
-            //left : 37 , right : 39
-              let status=  this.bar.update(this.bar_range); 
-              if(status == 0){
-                  this.bar_range = status;
-              }else if(status == window.innerWidth - this.bar_width){
-                  this.bar_range = status;
-              }
+        this.bar_yrange = this.bar_y;
 
-              if(e.keyCode == 37){
-                this.bar_range -=20;
-               }else if(e.keyCode == 39){
+        this.bar = new Bar(this.bar_x, this.bar_y, this.bar_width, this.bar_height, this.bar_range,this.bar_yrange);
+        window.addEventListener('keydown', (e) => {
+            //left : 37 , right : 39 ,top : 38 , bottom: 40
+            let status = this.bar.update(this.bar_range, this.bar_yrange);
+            if (status == 'minX') {
+                this.bar_range = 0;
+            } else if (status == 'maxX') {
+                this.bar_range = (window.innerWidth - this.bar_width);
+            } else if (status == 'minY') {
+                this.bar_yrange = 0;
+            } else if (status == 'maxY') {
+                this.bar_yrange = (window.innerHeight - this.bar_height);
+
+            }
+
+            if (e.keyCode == 37) {
+                this.bar_range -= 20;
+            } else if (e.keyCode == 39) {
                 this.bar_range += 20
-               }
+            } else if (e.keyCode == 38) {
+                this.bar_yrange -= 20;
+            } else if (e.keyCode == 40) {
+                this.bar_yrange += 20;
+            }
 
-               this.bar.update(this.bar_range)
-           
+            this.bar.update(this.bar_range, this.bar_yrange);
+
         })
 
         //shape
 
         this.shape = new Array();
-        this.shape_count = Math.floor(Math.random() * 50);
-        
-        for(let i = 0; i < this.shape_count ;i++){
+         this.shape_count = Math.floor(Math.random() * 10)+1;
+        //this.shape_count = 5;
+        for (let i = 0; i < this.shape_count; i++) {
             let x = Math.floor(Math.random() * window.innerWidth);
             let y = Math.floor(Math.random() * window.innerHeight);
-            let radius = Math.floor((Math.random()* 30)+15);
+            let radius = Math.floor((Math.random() * 30) + 15);
             let speed = 10;
             let r = Math.floor(Math.random() * 255);
             let g = Math.floor(Math.random() * 255);
             let b = Math.floor(Math.random() * 255);
             let color = `rgba(${r},${g},${b},0.5)`;
-            this.shape.push(new Shape(x,y,radius,speed,color));
-            this.shape[i].crashBar(this.bar_x,this.bar_y,this.bar_width,this.bar_height);
+            this.shape.push(new Shape(x, y, radius, speed, color));
+
         }
 
-        
+
     }
 
-    resize(){
+    resize() {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-        
+
     }
-    animate(){
-        this.ctx.clearRect(0,0,window.innerWidth,window.innerHeight)
+    animate() {
+        this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
         window.requestAnimationFrame(this.animate.bind(this));
-        for(let i = 0 ;i < this.shape.length ; i++){
+        for (let i = 0; i < this.shape.length; i++) {
             this.shape[i].draw(this.ctx);
+            this.shape[i].crashBar(this.bar.x, this.bar.y, this.bar.width, this.bar.height);
         }
 
-        
+
         this.bar.move(this.ctx);
+
     }
 }
 
 
 
-window.onload = ()=>{
+window.onload = () => {
     new App();
 }
